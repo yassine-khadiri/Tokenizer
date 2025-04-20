038d714337de5dad32a1253dd05d7524fa146ec4
yassine-khadiri <yassinekhadiri77@gmail.com>
2025-04-20 17:05:44 +0100

    Contract Needs To Be Uploaded

--- code/SamerV42Token.sol
+++ code/SamerV42Token.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.0 <0.9.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SamerV42Token is ERC20, Ownable {
 
     uint256 public constant MAX_SUPPLY = 1337 * 10**18;
+    uint256 public constant INITIAL_SUPPLY = 1000 * 10**18;
 
     constructor() ERC20("SamerV42", "SV42") Ownable(msg.sender) {
-        _mint(msg.sender, MAX_SUPPLY);
+        _mint(msg.sender, INITIAL_SUPPLY);
     }
 
      // Only owner can mint
